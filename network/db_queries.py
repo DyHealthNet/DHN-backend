4094d5f8b2cfd92aa3d43808b46692455037fa03
Andrea Raithel <andrea.raithel@tum.de>
2024-07-06 23:56:14 +0200

    fix django ORM queries

--- network/db_queries.py
+++ network/db_queries.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from django.apps import apps
 from django.db import connection
 import timeit
+import json
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dyhealthnet_project.settings')
 django.setup()
@@ -72,33 +73,47 @@ def get_tables(node_type):
 
 
 def query_identity_table(table, id_names, base_table, query_id, limit):
+    # Account for differences in table names vs. model names
     parts = table.split('_')
     table = ''.join([part.capitalize() for part in parts])
     table_model = apps.get_model('network', table.capitalize())
-    if table_model.__name__.startswith('Effects'):
-        order = 'p_value'
-    else:
-        order = None
 
-    queryset = table_model.objects.select_related(id_names[0]).select_related(id_names[1])
-    results = queryset.filter(Q(**{id_names[0]: query_id}) | Q(**{id_names[1]: query_id}))
-    if order:
-        results = results.order_by(order)
-    return results[:limit]
+    base_table_name = base_table[0].capitalize()
+    base_table_model = apps.get_model('network', base_table_name.capitalize())
+
+    # Filter for query ID
+    edges_queryset = table_model.objects.filter(Q(**{id_names[0]: query_id}) | Q(**{id_names[1]: query_id}))
+
+    # Optional: order by p-value
+    if table_model.__name__.startswith('Effects'):
+        edges_queryset = edges_queryset.order_by('p_value')
+
+    # Limit and evaluate
+    edges = edges_queryset[:limit].values()
+
+    # Retrieve corresponding node information via primary keys
+    unique_ids = set()
+    for edge in edges:
+        unique_ids.add(edge.get(f'{id_names[0]}_id'))
+        unique_ids.add(edge.get(f'{id_names[1]}_id'))
+
+    nodes = base_table_model.objects.filter(pk__in=unique_ids).values()
+
+    return nodes, edges
 
 def get_remaining_tables(table, id_names, base_table, query_id, limit):
+    # Account for differences in table names vs. model names
     parts = table.split('_')
-    table = ''.join([part.capitalize() for part in parts])
-    table_model = apps.get_model('network', table.capitalize())
-    if table_model.__name__.startswith('Effects'):
-        order = 'p_value'
-    else:
-        order = None
+    table_new = ''.join([part.capitalize() for part in parts])
+    table_model = apps.get_model('network', table_new.capitalize())
 
+    # Find second base_table
+    if 'associates' in table: # fixed a weird error, probably some inconsistency in the database
+        table = table[:-1]
     table_parts = tuple([x for x in table.split('_') if x not in {'effects', 'associates', 'affects'}])
     table_keys = TABLE_IDS.get(table_parts)
     if not table_keys:
-        return {}
+        return [], []
 
     second_base = None
     for name, domain_id in zip(table_parts, table_keys):
@@ -107,14 +122,35 @@ def get_remaining_tables(table, id_names, base_table, query_id, limit):
             second_base = (f"{name}s", domain_id)
             break
     if not second_base:
-        return {}
+        return [], []
 
-    queryset = table_model.objects.select_related(id_names[0]).select_related(second_base[1])
-    results = queryset.filter(Q(**{id_names[0]: query_id}))
+    first_base_table_name = base_table[0].capitalize()
+    first_base_table_model = apps.get_model('network', first_base_table_name.capitalize())
+    second_base_table_name = second_base[0].capitalize()
+    second_base_table_model = apps.get_model('network', second_base_table_name.capitalize())
 
-    if order:
-        results = results.order_by(order)
-    return results[:limit]
+    # Filter for query ID
+    edges_queryset = table_model.objects.filter(Q(**{id_names[0]: query_id}) | Q(**{second_base[1]: query_id}))
+
+    # Optional: order by p-value
+    if table_model.__name__.startswith('Effects'):
+        edges_queryset = edges_queryset.order_by('p_value')
+
+    # Limit and evaluate
+    edges = edges_queryset[:limit].values()
+
+    # Retrieve corresponding node information via primary keys
+    unique_ids_first_base = set()
+    unique_ids_second_base = set()
+    for edge in edges:
+        unique_ids_first_base.add(edge.get(f'{id_names[0]}_id'))
+        unique_ids_second_base.add(edge.get(f'{second_base[1]}_id'))
+
+    nodes = []
+    nodes += first_base_table_model.objects.filter(pk__in=unique_ids_first_base).values()
+    nodes += second_base_table_model.objects.filter(pk__in=unique_ids_second_base).values()
+
+    return nodes, edges
 
 def get_edges(node_type, query_id, limit=100):
     # we differentiate between the base table, identity tables, and remaining tables
@@ -128,26 +164,36 @@ def get_edges(node_type, query_id, limit=100):
     # remaining tables is like [('effects_protein_metabolite', ['uniprot_id']), ...]
 
     # query the identity tables
-    results = {}
+    nodes_results = {}
+    edges_results = {}
+    #results = {}
     for table, id_names in identity_tables:
-        result = query_identity_table(table, id_names, base_table, query_id, limit)
-        results[table] = result
+        nodes, edges = query_identity_table(table, id_names, base_table, query_id, limit)
+        nodes_results[table] = nodes
+        edges_results[table] = edges
 
     for table, id_name in remaining_tables:
-        result = get_remaining_tables(table, id_name, base_table, query_id, limit)
-        results[table] = result
+        nodes, edges = get_remaining_tables(table, id_name, base_table, query_id, limit)
+        nodes_results[table] = nodes
+        edges_results[table] = edges
 
     # get the sum of the results
-    total_results = sum([len(result) for result in results.values()])
-    return results, total_results
+    total_edges_results = sum(len(edges) for edges in edges_results.values())
+    total_nodes_results = sum(len(nodes) for nodes in nodes_results.values())
+    return nodes_results, edges_results, total_edges_results, total_nodes_results
 
 if __name__ == '__main__':
     start = timeit.default_timer()
-    edges, num_edges = get_edges('uniprot_id', 'uniprot.P31946', limit=10)
-    print(f"Took {timeit.default_timer() - start:.2f} seconds to get {num_edges} edges")
-    for objects in edges.values():
-        for edges in objects:
-            print(edges.p_value)
-            print(edges.effect_size_type)
-            print(edges.uniprot_id_1)
-            print(edges.uniprot_id_2)
+    nodes, edges, num_edges, num_nodes = get_edges('uniprot_id', 'uniprot.P31946', limit=100)
+    print(f"Took {timeit.default_timer() - start:.2f} seconds to get {num_edges} edges and {num_nodes} nodes.")
+
+    print('Edges:')
+    for table, objects in edges.items():
+        print(table, objects)
+        #for result in table:
+        #    print(result)
+
+    print('Nodes:')
+    for table in nodes.values():
+        for result in table:
+            print(result)
