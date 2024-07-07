1a490e54a6a4a0c6c4f18e5ce5c2f836ea10b4cb
Andrea Raithel <andrea.raithel@tum.de>
2024-07-07 23:08:35 +0200

    update api endpoint descriptions

--- network/views.py
+++ network/views.py
@@ -93,8 +93,8 @@ class Detail_EdgeView(generic.DetailView):
 
 @extend_schema_view(
     get=extend_schema(
-        summary="Returns all network edges whilst giving for each node (foreign key) its description",
-        description="""Returns all network edges whilst giving for each node (foreign key) its description
+        summary="Returns all network edges and the descriptions of their corresponding edges (foreign key)",
+        description="""Returns all network edges and the descriptions of their corresponding edges (foreign key)
             e.g. [{"node1__description_text":"Pro (Metabolite)","node2__description_text":"PC aa C34:4 (Metabolite)",
             "score":"0.1104","effect_size":"-0.6072"},{"node1__description_text":"Pro (Metabolite)",
             "node2__description_text":"Pro (Metabolite)","score":"534.5000","effect_size":"684.0000"}]
@@ -179,16 +179,9 @@ class GetVariablesView(generics.GenericAPIView):
 
 @extend_schema_view(
     get=extend_schema(
-        summary="Returns the data out of the given variables x (e.g. time), y (dosage) and c(drug) in JSON format",
-        description="""Returns the data for the given variables x, y, and c in JSON format. 
-            e.g. {"labels": ["18:00","18:30","19:00","19:30","20:00","20:30","21:00"], 
-            # x var values "datasets": [{
-                "label": "Ibuprofen",
-                    "backgroundColor": "pink",
-                    "data": [0, 20,40, 65, 70, 75, 80]},{      # y var values of c var group "Iboprofen"
-                "label": "Aspirin",
-                    "backgroundColor": "blue",
-                    "data": [0, 10,20, 30, 40, 45, 50]}]}      # y var values of c var group "Aspirin"
+        summary="Returns averaged data for the given variables x and y grouped by c in JSON format",
+        description="""Returns averaged data for the given variables x (e.g. time) and y (e.g. dosage) in JSON format.
+            The optional parameter c (e.g. sex) allows for comparisons between different groups such as males and females.
             """,
         parameters=[
             OpenApiParameter(
@@ -279,6 +272,38 @@ class GetDataView(generics.GenericAPIView):
         req_data_dict["datasets"] = temp
         return JsonResponse(req_data_dict, safe=True)
 
+#TODO
+@extend_schema_view(
+    get=extend_schema(
+        summary="Returns boxplot data for the given variables x and y grouped by c in JSON format",
+        description="""Returns boxplot data for the given variables x (e.g. time) and y (e.g. dosage) in JSON format.
+            The optional parameter c (e.g. sex) allows for comparisons between different groups such as males and females.
+            """,
+        parameters=[
+            OpenApiParameter(
+                name='x',
+                description='X parameter',
+                required=True,
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.QUERY,
+            ),
+            OpenApiParameter(
+                name='y',
+                description='Y parameter',
+                required=True,
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.QUERY,
+            ),
+            OpenApiParameter(
+                name='c',
+                description='C parameter',
+                required=False,
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.QUERY,
+            )
+        ],
+    )
+)
 class GetDataBoxPlotView(generics.GenericAPIView):
     def get(self, request):
         # Get request vars
