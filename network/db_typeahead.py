f80994319d5fd81ebbe5f9aecc70bdaf5a675f9e
Andrea Raithel <andrea.raithel@tum.de>
2024-07-29 17:53:11 +0200

    update typeahead query and add first step of network query

--- network/db_typeahead.py
+++ network/db_typeahead.py
@@ -2,30 +2,22 @@ import os
 import django
 from django.db.models import Q
 from django.apps import apps
-from django.db import connection
 import timeit
-import json
-from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dyhealthnet_project.settings')
 django.setup()
 
-def query_description(query):
+def typeahead_query(query):
     model = apps.get_model('network', 'ViewDescriptionFTS')
-    return model.objects.filter(description__search=query).values()
-
-def query_display(query):
-    model = apps.get_model('network', 'ViewDescriptionFTS')
-    return model.objects.filter(display_name__icontains=query).values()
+    return model.objects.filter(Q(description__icontains=query) |
+                                Q(display_name__icontains=query) |
+                                Q(id__icontains=query)).values()
 
 if __name__ == '__main__':
     start = timeit.default_timer()
-    descriptions = query_description('low body')
-    display_names = query_display('brca')
-    print(f"Took {timeit.default_timer() - start:.2f} seconds.")
+    results = typeahead_query('high')
+    print(f"Took {timeit.default_timer() - start} seconds.")
 
-    print('Descriptions:')
-    print(descriptions)
-
-    print('Display names:')
-    print(display_names)
+    print('Results:')
+    for entry in results:
+        print(entry)
