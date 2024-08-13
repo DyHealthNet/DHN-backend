c0b69c83e8eb5d1562f7c30f0032b0e0f3d2b1d7
Andrea Raithel <andrea.raithel@tum.de>
2024-08-13 13:04:09 +0200

    uncomment templates (thought earlier that it was unnecessary but it created an error while opening swagger-ui)

--- dyhealthnet_project/settings.py
+++ dyhealthnet_project/settings.py
@@ -79,22 +79,22 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'dyhealthnet_project.urls'
 
-#TEMPLATES = [
-#    {
-#        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#        'DIRS': [BASE_DIR / 'templates']
-#        ,
-#        'APP_DIRS': True,
-#        'OPTIONS': {
-#            'context_processors': [
-#                'django.template.context_processors.debug',
-#                'django.template.context_processors.request',
-#                #'django.contrib.auth.context_processors.auth',
-#                'django.contrib.messages.context_processors.messages',
-#            ],
-#        },
-#    },
-#]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates']
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                #'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = 'dyhealthnet_project.wsgi.application'
 
